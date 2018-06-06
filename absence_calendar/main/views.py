bd9581be17e7f38c244cdf137477032c724df778
44Schwarz <sirmitya@gmail.com>
2018-06-06 22:14:34 +0300

    Add the url and the view in the main app

--- absence_calendar/main/views.py
+++ absence_calendar/main/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+
 
 # Create your views here.
+def calendar_view(request):
+    return HttpResponse('There will be a calendar')
